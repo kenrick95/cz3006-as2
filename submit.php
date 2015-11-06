fc44d42dd5556e86c1cb3f94b73e8810f7115811
kenrick95 <kenrick95@gmail.com>
2015-11-06 22:23:44 +0800

    spacing

--- submit.php
+++ submit.php
@@ -17,16 +17,16 @@ $contents = "";
 $old_apples = $old_oranges = $old_bananas = 0;
 
 if (file_exists($filename)) {
-    // read from file until end of file
-    $contents = fread($handle, filesize($filename) + 1);
-    preg_match("/Total number of apples: (\d+)/", $contents, $pm_apple);
-    preg_match("/Total number of oranges: (\d+)/", $contents, $pm_orange);
-    preg_match("/Total number of bananas: (\d+)/", $contents, $pm_banana);
+  // read from file until end of file
+  $contents = fread($handle, filesize($filename) + 1);
+  preg_match("/Total number of apples: (\d+)/", $contents, $pm_apple);
+  preg_match("/Total number of oranges: (\d+)/", $contents, $pm_orange);
+  preg_match("/Total number of bananas: (\d+)/", $contents, $pm_banana);
 
-    // get integer value
-    $old_apples = intval($pm_apple[1]);
-    $old_oranges = intval($pm_orange[1]);
-    $old_bananas = intval($pm_banana[1]);
+  // get integer value
+  $old_apples = intval($pm_apple[1]);
+  $old_oranges = intval($pm_orange[1]);
+  $old_bananas = intval($pm_banana[1]);
 
 }
 // update value
@@ -49,43 +49,61 @@ fclose($handle);
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>CZ3006 Assignment 2</title>
-    </head>
-    <body>
-            <h1>Receipt</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <td colspan="2">Name: <?php echo $name; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Apples<br>&emsp; <?php echo $apples; ?>&times; &cent;69</td>
-                        <td style="vertical-align: top;">$<?php echo $apples * .69; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Oranges<br>&emsp; <?php echo $oranges; ?> &times; &cent;59</td>
-                        <td style="vertical-align: top;">$<?php echo $oranges * .59; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Bananas<br>&emsp; <?php echo $bananas; ?> &times; &cent;39</td>
-                        <td style="vertical-align: top;">$<?php echo $bananas * .39; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Total:</td>
-                        <td style="border-top: 1px solid #000;">$<?php echo $apples * .69 + $oranges * .59 + $bananas * .39; ?></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">Payment method: <?php echo $payment; ?>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            Thank you for shopping with us.
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <title>CZ3006 Assignment 2</title>
+</head>
+<body>
+  <h1>Receipt</h1>
+  <table>
+    <tbody>
+      <tr>
+        <td colspan="2">Name: <?php echo $name; ?></td>
+      </tr>
+      <tr>
+        <td>
+          Apples<br>
+          &emsp; <?php echo $apples; ?>&times; &cent;69
+        </td>
+        <td style="vertical-align: top;">
+          $<?php echo $apples * .69; ?>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Oranges<br>
+          &emsp; <?php echo $oranges; ?> &times; &cent;59
+        </td>
+        <td style="vertical-align: top;">
+          $<?php echo $oranges * .59; ?>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          Bananas<br>
+          &emsp; <?php echo $bananas; ?> &times; &cent;39
+        </td>
+        <td style="vertical-align: top;">
+          $<?php echo $bananas * .39; ?>
+        </td>
+      </tr>
+      <tr>
+        <td>Total:</td>
+        <td style="border-top: 1px solid #000;">
+          $<?php echo $apples * .69 + $oranges * .59 + $bananas * .39; ?>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          Payment method: <?php echo $payment; ?>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="2">
+          Thank you for shopping with us.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
 </html>
