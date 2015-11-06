82e6a8cf917933cc8d9fb5f94271da010de42e00
kenrick95 <kenrick95@gmail.com>
2015-11-06 22:09:07 +0800

    comments

--- submit.php
+++ submit.php
@@ -1,20 +1,5 @@
 <?php
-/*
-2)	Write a server-side PHP program that receives the user’s order when the
-Submit button on the above HTML document is clicked. On receiving the order,
- the server-side PHP program computes the total cost of the user’s order and
- returns an HTML document to the user as a receipt. The receipt should specify
- the user’s name, what are ordered and the payment method in the form of a
- table. In addition, the server-side PHP program must also update a file named
- “order.txt” stored on the web server to reflect the new order. The file
- records the total numbers of apples, oranges and bananas ordered by all
- users so far in the following format:
-
-Total number of apples: 12
-Total number of oranges: 23
-Total number of bananas: 35
-
-*/
+// Handle POST request, if not set, set as empty string or 0.
 $name  = isset($_POST['name']) ? $_POST['name'] : "";
 
 $apples  = isset($_POST['apples']) ? $_POST['apples'] : 0;
@@ -23,34 +8,42 @@ $bananas = isset($_POST['bananas']) ? $_POST['bananas'] : 0;
 
 $payment = isset($_POST['payment']) ? $_POST['payment'] : "";
 
+// opens order.txt
 $filename = "order.txt";
-$handle = fopen($filename, "c+");
+$handle = fopen($filename, "c+"); // c+: read & write; create if not exist
 $contents = "";
 
+// initialize quantity
 $old_apples = $old_oranges = $old_bananas = 0;
 
 if (file_exists($filename)) {
+    // read from file until end of file
     $contents = fread($handle, filesize($filename) + 1);
     preg_match("/Total number of apples: (\d+)/", $contents, $pm_apple);
     preg_match("/Total number of oranges: (\d+)/", $contents, $pm_orange);
     preg_match("/Total number of bananas: (\d+)/", $contents, $pm_banana);
 
+    // get integer value
     $old_apples = intval($pm_apple[1]);
     $old_oranges = intval($pm_orange[1]);
     $old_bananas = intval($pm_banana[1]);
 
 }
+// update value
 $new_apples = $old_apples + $apples;
 $new_oranges = $old_oranges + $oranges;
 $new_bananas = $old_bananas + $bananas;
 
+// new string to print
 $msg = "Total number of apples: $new_apples\n";
 $msg .= "Total number of oranges: $new_oranges\n";
 $msg .= "Total number of bananas: $new_bananas\n";
 
+// seek to head of file, cause file buffer has reached end of file
 fseek($handle, 0);
 fwrite($handle, $msg);
 
+// don't forget to close the file
 fclose($handle);
 
 ?>
@@ -65,26 +58,26 @@ fclose($handle);
             <table>
                 <tbody>
                     <tr>
-                        <td colspan="2">Name: <?= $name ?></td>
+                        <td colspan="2">Name: <?php echo $name; ?></td>
                     </tr>
                     <tr>
-                        <td>Apples<br>&emsp; <?= $apples ?>&times; &cent;69</td>
-                        <td style="vertical-align: top;">$<?= $apples * .69 ?></td>
+                        <td>Apples<br>&emsp; <?php echo $apples; ?>&times; &cent;69</td>
+                        <td style="vertical-align: top;">$<?php echo $apples * .69; ?></td>
                     </tr>
                     <tr>
-                        <td>Oranges<br>&emsp; <?= $oranges ?> &times; &cent;59</td>
-                        <td style="vertical-align: top;">$<?= $oranges * .59 ?></td>
+                        <td>Oranges<br>&emsp; <?php echo $oranges; ?> &times; &cent;59</td>
+                        <td style="vertical-align: top;">$<?php echo $oranges * .59; ?></td>
                     </tr>
                     <tr>
-                        <td>Bananas<br>&emsp; <?= $bananas ?> &times; &cent;39</td>
-                        <td style="vertical-align: top;">$<?= $bananas * .39 ?></td>
+                        <td>Bananas<br>&emsp; <?php echo $bananas; ?> &times; &cent;39</td>
+                        <td style="vertical-align: top;">$<?php echo $bananas * .39; ?></td>
                     </tr>
                     <tr>
                         <td>Total:</td>
-                        <td style="border-top: 1px solid #000;">$<?= $apples * .69 + $oranges * .59 + $bananas * .39 ?></td>
+                        <td style="border-top: 1px solid #000;">$<?php echo $apples * .69 + $oranges * .59 + $bananas * .39; ?></td>
                     </tr>
                     <tr>
-                        <td colspan="2">Payment method: <?= $payment ?>
+                        <td colspan="2">Payment method: <?php echo $payment; ?>
                         </td>
                     </tr>
                     <tr>
